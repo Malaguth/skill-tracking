0239b9b028e18c1f74a743ae59ce35d57619a636
JoaoColem <joaocostaval@gmail.com>
2024-02-01 14:57:24 -0300

    remove comments

--- frontend/src/features/Counter/Counter.styles.ts
+++ frontend/src/features/Counter/Counter.styles.ts
@@ -4,13 +4,11 @@ import DefaultButton from '../../components/Buttons/DefaultButton';
 export const IncrementButton = styled(DefaultButton)`
   && {
     background-color: red;
-    // Outras propriedades de estilo necessárias
   }
 `;
 
 export const DecrementButton = styled(DefaultButton)`
   && {
     color: green;
-    // Outras propriedades de estilo necessárias
   }
 `;
